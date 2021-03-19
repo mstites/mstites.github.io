9a673611953d9583713ef2dd707f922568b63bc7
mstites <maeve@mstites.com>
2021-03-18 23:10:00 -0400

    Put patch panel project content on first page style

---
+++ js/items.js
@@ -0,0 +1,23 @@
+// Much of the same code as the index.js, just with corrected link locations and
+// removing the portfolio items
+
+var work = document.getElementById("portfolioItemContainer");
+var workRequest = new XMLHttpRequest();
+
+var link = document.getElementById("socialLinkContent");
+var linkRequest = new XMLHttpRequest();
+
+linkRequest.open("GET", "../json/links.json", true);
+linkRequest.onreadystatechange = function () {
+    if (linkRequest.readyState === 4) {
+        if (linkRequest.status === 200 || linkRequest.status == 0) {
+            var html = "";
+            JSON.parse(linkRequest.responseText).forEach(function (link) {
+                console.log(link.name);
+                html += "<div class=\"links waves-effect\"><a href=" + link.url + " class=\"black-text valign-wrapper\" target=\"_blank\"><img class=\"activator\" src=../" + link.icon + "></i>&nbsp; " + link.name + "</a></div>";
+            });
+            link.innerHTML = html;
+        }
+    }
+};
+linkRequest.send(null);
